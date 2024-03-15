bd19f540b1a5ca24d06d70232ff869390dfd296f
vin <hydedada0129@gmail.com>
2024-03-15 22:32:07 +0800

    add printf "first change"

--- src/testgit/Test1.java
+++ src/testgit/Test1.java
@@ -5,6 +5,9 @@ class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GITTEST~~~~~~~!!!!");
+		
+		//add another printf
+		System.out.println("first change ");
 	}
 
 }
