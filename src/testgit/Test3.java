78b4eb0171289eaa2e991958e7f3b742445f16d8
vin <hydedada0129@gmail.com>
2024-03-17 22:06:02 +0800

    commiting Test3 to BranchY

---
+++ src/testgit/Test3.java
@@ -0,0 +1,13 @@
+package testgit;
+
+class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GITTEST~~~~~~~!!!!");
+		
+		//add another printf
+		System.out.println("first change ");
+	}
+
+}
